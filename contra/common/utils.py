48c96c672467e1ae152053bb5f5681d02a3390b9
Uriel Singer <usinger@ebay.com>
2020-12-09 20:11:27 +0200

    CUI evaluation support

---
+++ contra/common/utils.py
@@ -0,0 +1,14 @@
+import os
+import torch
+
+from contra.constants import SAVE_PATH
+
+def get_bert_model(date):
+    return os.path.join(SAVE_PATH, date.year)
+
+def mean_pooling(model_output, attention_mask):
+    token_embeddings = model_output[0]
+    input_mask_expanded = attention_mask.unsqueeze(-1).expand(token_embeddings.size()).float()
+    sum_embeddings = torch.sum(token_embeddings * input_mask_expanded, 1)
+    sum_mask = torch.clamp(input_mask_expanded.sum(1), min=1e-9)
+    return sum_embeddings / sum_mask
