c77d7bc9de1761e34d01ba7505803cc361207165
Shunit Agmon <shunit.agmon@gmail.com>
2020-12-20 13:14:03 +0200

    fix a problem with multiple GPUs (prepare_data vs setup)

--- contra/config.py
+++ contra/config.py
@@ -16,13 +16,17 @@ parser.add_argument('--start_year', default=2018, type=int,
                     help='start year for BERT pretraining')
 parser.add_argument('--end_year', default=2018, type=int,
                     help='end year for BERT pretraining')
+
+parser.add_argument('--by_sentence', dest='by_sentence', action='store_true')
+parser.add_argument('--no-by_sentence', dest='by_sentence', action='store_false')
+
 parser.add_argument('--min_num_participants', default=1, type=int, help='minimum number of total participants')
 parser.add_argument('--train_test_split', default=0.8, type=float, help='train_test_split ratio')
 
 parser.add_argument('--name', type=str, help='a special name for the current model running')
 parser.add_argument('--save_path', metavar='DIR', default=SAVE_PATH, type=str, help='path to save model')
 parser.add_argument('--log_path', default=LOG_PATH, type=str, help='tensorboard log path')
-parser.add_argument('--gpus', default='0', help='gpus parameter used for pytorch_lightning') # default='0'
+parser.add_argument('--gpus', default='0', nargs='+', help='gpus parameter used for pytorch_lightning')
 
 parser.add_argument('--lr', '--learning_rate', default=1e-4, type=float,
                     metavar='LR', help='initial learning rate', dest='learning_rate')
